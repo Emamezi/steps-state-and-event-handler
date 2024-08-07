f407aa6c2a0fab4d1314bf4a367fef3b68fa3e30
Mezi <emameziebebinwe@gmail.com>
2024-08-07 10:56:55 -0400

    added open and close modal functionality

--- src/App.js
+++ src/App.js
@@ -7,42 +7,51 @@ const messages = [
 ];
 function App() {
   const [step, setStep] = useState(1);
-  const [test, setTest] = useState({ name: "Fred" });
+  const [isOpen, setIsOpen] = useState(true);
 
   const previousButtonHandler = () => {
     if (step > 1) setStep(step - 1);
   };
   const nextButtonHandler = () => {
     if (step < 3) setStep(step + 1);
-    setTest({ name: "Mezi" });
+  };
+
+  const closeStepHandler = () => {
+    setIsOpen(!isOpen);
   };
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
+    <div>
+      <button className="close" onClick={closeStepHandler}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
 
-      <p className="message">
-        Step{step}: {messages[step - 1]}
-        {test.name}
-      </p>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={previousButtonHandler}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={nextButtonHandler}
-        >
-          Next
-        </button>
-      </div>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={previousButtonHandler}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={nextButtonHandler}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
