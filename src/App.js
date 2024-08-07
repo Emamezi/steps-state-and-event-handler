c33ec73f636f5b33eca3cf323027b06e580da0c1
Mezi <emameziebebinwe@gmail.com>
2024-08-06 23:37:18 -0400

    adding intial files for steps state and event handler

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+function App() {
+  const [step, setStep] = useState(1);
+  const [test, setTest] = useState({ name: "Fred" });
+
+  const previousButtonHandler = () => {
+    if (step > 1) setStep(step - 1);
+  };
+  const nextButtonHandler = () => {
+    if (step < 3) setStep(step + 1);
+    setTest({ name: "Mezi" });
+  };
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step{step}: {messages[step - 1]}
+        {test.name}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={previousButtonHandler}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={nextButtonHandler}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
