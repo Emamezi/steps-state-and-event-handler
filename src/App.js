9a55fffbecd93de60571f533549b0d1d42518544
Mezi <emameziebebinwe@gmail.com>
2024-08-14 13:14:45 -0400

    code refactoring

--- src/App.js
+++ src/App.js
@@ -31,11 +31,7 @@ function App() {
             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
-
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button
               bgColor="#7950f2"
@@ -62,6 +58,15 @@ function App() {
 }
 
 export default App;
+
+const StepMessage = ({ step, children }) => {
+  return (
+    <div className="message">
+      <h3> Step {step} </h3>
+      {children}
+    </div>
+  );
+};
 
 const Button = ({ bgColor, textColor, onClick, children }) => {
   return (
